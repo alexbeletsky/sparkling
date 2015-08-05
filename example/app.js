dd5b01018b3b8c7f5cecde4f933dba6ed80d9325
alexbeletsky <alexander.beletsky@gmail.com>
2015-08-05 10:23:47 +0200

    added mongodb oplog listener

--- example/app.js
+++ example/app.js
@@ -3,11 +3,14 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var morgan = require('morgan');
 
+var mongo = 'mongodb://localhost:27017/sparklingdb';
+
 var sparkling = require('../');
 
 var app = express();
-var reactive = sparkling(app);
+var reactive = sparkling(app, {mongo: {connection: mongo, db: 'sparklingdb'}});
 
+app.use(morgan('combined'));
 app.use(reactive());
 app.use(bodyParser.json());
 app.use(methodOverride());
--- source/index.js
+++ source/index.js
@@ -1,7 +1,11 @@
 var http = require('http');
 var DdpServer = require('ddp-server');
+var MongoOplog = require('mongo-oplog');
 
-function sparkling(app, methods) {
+function sparkling(app, options) {
+    if (!options || !options.mongo) {
+        throw new Error('missing mongo configuration');
+    }
 
     function factory() {
         return function(req, res, next) {
@@ -12,12 +16,21 @@ function sparkling(app, methods) {
     app.listen = function () {
         var server = http.createServer(this);
         var ddp = new DdpServer({server: server});
+        var oplog = new MongoOplog(options.mongo.connection, {ns: options.mongo.db}).tail();
 
         ddp.methods({
             test: function () {
                 console.log('ddp - test method called');
                 return true;
             }
+        });
+
+        oplog.on('op', function (e) {
+            console.log(e);
+        });
+
+        oplog.on('error', function (e) {
+            console.error(e);
         });
 
         return server.listen.apply(server, arguments);

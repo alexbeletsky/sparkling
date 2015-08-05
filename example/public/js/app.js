cc50afb067f100b00a899e6c27d6ea23107a09d5
alexbeletsky <alexander.beletsky@gmail.com>
2015-08-05 17:48:17 +0200

    recieve event on client

--- example/public/js/app.js
+++ example/public/js/app.js
@@ -1,30 +1,22 @@
 // import sparkling from '../../../client';
 
-var DDPClient = require('ddp');
+var DDP = require('ddp.js');
 
-var client = new DDPClient({
-    host : "localhost",
-    port : 3000,
-    ssl  : false,
-    autoReconnect : true,
-    autoReconnectTimer : 500,
-    maintainCollections : true,
-    ddpVersion : '1'
-});
+var options = {
+    endpoint: 'ws://localhost:3000/websocket',
+    SocketConstructor: WebSocket
+};
 
-client.connect(function (err, connected) {
-    if (err) {
-        throw err;
-    }
+var ddp = new DDP(options);
 
-    client.subscribe('accounts', [], function (err) {
-        console.log('subscribe - accounts', err, client.collections);
+ddp.on('connected', function () {
+    ddp.on('ready', function (msg) {
+        console.log('ready', msg);
     });
 
-    var observable = client.observe('accounts');
-    observable.changed = function(id, oldFields, clearedFields) {
-        console.log("[CHANGED] in " + observable.name + ":  " + id);
-        console.log("[CHANGED] old field values: ", oldFields);
-        console.log("[CHANGED] cleared fields: ", clearedFields);
-    };
+    ddp.on('changed', function (msg) {
+        console.log('changed', msg);
+    });
+
+    ddp.sub('accounts', {user: 'test@user.com'});
 });

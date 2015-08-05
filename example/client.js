e3014f6df694630c77b9d79a591adba8e15dfc34
alexbeletsky <alexander.beletsky@gmail.com>
2015-08-05 17:29:54 +0200

    reimplemented client with ddp.js

--- example/client.js
+++ example/client.js
@@ -1,28 +1,51 @@
-var DDPClient = require('ddp');
+var DDP = require('ddp.js');
+var webSocket = require('faye-websocket');
 
-var client = new DDPClient({
-    host : "localhost",
-    port : 3000,
-    ssl  : false,
-    autoReconnect : true,
-    autoReconnectTimer : 500,
-    maintainCollections : true,
-    ddpVersion : '1'
-});
+var options = {
+    endpoint: 'http://localhost:3000/websocket',
+    SocketConstructor: webSocket.Client
+};
 
-client.connect(function (err, connected) {
-    if (err) {
-        throw err;
-    }
+var ddp = new DDP(options);
 
-    client.subscribe('accounts', [], function (err) {
-        console.log('subscribe - accounts', err, client.collections);
+ddp.on('connected', function () {
+    ddp.on('ready', function (msg) {
+        console.log('ready', msg);
     });
 
-    var observable = client.observe('accounts');
-    observable.changed = function(id, oldFields, clearedFields) {
-        console.log("[CHANGED] in " + observable.name + ":  " + id);
-        console.log("[CHANGED] old field values: ", oldFields);
-        console.log("[CHANGED] cleared fields: ", clearedFields);
-    };
+    ddp.on('changed', function (msg) {
+        console.log('changed', msg);
+    });
+
+    ddp.sub('accounts', {user: 'test@user.com'});
 });
+
+
+// var DDPClient = require('ddp');
+//
+// var client = new DDPClient({
+//     host : "localhost",
+//     port : 3000,
+//     ssl  : false,
+//     autoReconnect : true,
+//     autoReconnectTimer : 500,
+//     maintainCollections : true,
+//     ddpVersion : '1'
+// });
+//
+// client.connect(function (err, connected) {
+//     if (err) {
+//         throw err;
+//     }
+//
+//     client.subscribe('accounts', [], function (err) {
+//         console.log('subscribe - accounts', err, client.collections);
+//     });
+//
+//     var observable = client.observe('accounts');
+//     observable.changed = function(id, oldFields, clearedFields) {
+//         console.log("[CHANGED] in " + observable.name + ":  " + id);
+//         console.log("[CHANGED] old field values: ", oldFields);
+//         console.log("[CHANGED] cleared fields: ", clearedFields);
+//     };
+// });
